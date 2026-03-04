47feb1e75b0f6769818f2c16565f292169ff0fdc
Shreeya-1030 <shreeya.k05@gmail.com>
2026-03-04 09:37:30 +0530

    Update clock_tb.sv

--- clock_tb.sv
+++ clock_tb.sv
@@ -1,3 +1,11 @@
+//File: clock_tb.sv
+//Author: Shreeya Sachin Kirdat / 1BM23EC249
+//Created: 25/02/2026
+//Module: Digital CLock
+//Project: SystemVerilog and Verification (23EC6PE2SV)
+//	 Faculty: Prof. Ajaykumar Devarapalli
+//Description: A simple SystemVerilog top module for a digital clock to count upto 59:59.
+
 `timescale 1ns/1ps
 
 module clock_tb;
