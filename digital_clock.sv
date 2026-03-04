a31225a93da33926423871e252132051f1cde75d
Shreeya-1030 <shreeya.k05@gmail.com>
2026-03-04 09:34:21 +0530

    Update digital_clock.sv

--- digital_clock.sv
+++ digital_clock.sv
@@ -1,3 +1,11 @@
+//File: digital_clock.sv
+//Author: Shreeya Sachin Kirdat / 1BM23EC249
+//Created: 25/02/2026
+//Module: Digital CLock
+//Project: SystemVerilog and Verification (23EC6PE2SV)
+//	 Faculty: Prof. Ajaykumar Devarapalli
+//Description: A simple SystemVerilog code for a digital clock to count upto 59:59.
+
 `timescale 1ns/1ps
 
 module digital_clock (
