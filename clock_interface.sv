6e7fed139af4173a05abf064eb69b8421dacaa87
Shreeya-1030 <shreeya.k05@gmail.com>
2026-03-04 09:36:38 +0530

    Update clock_interface.sv

--- clock_interface.sv
+++ clock_interface.sv
@@ -1,3 +1,11 @@
+//File: clock_interface.sv
+//Author: Shreeya Sachin Kirdat / 1BM23EC249
+//Created: 25/02/2026
+//Module: Digital CLock
+//Project: SystemVerilog and Verification (23EC6PE2SV)
+//	 Faculty: Prof. Ajaykumar Devarapalli
+//Description: A simple SystemVerilog interface block (with modport) code for a digital clock to count upto 59:59.
+
 `timescale 1ns/1ps
 
 interface clock_if (input logic clk);
